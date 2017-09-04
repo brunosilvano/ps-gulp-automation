fa0b6848ef39bb7dcaf3144c500c6ae441e359d6
Bruno Silvano <bruno_705@hotmail.com>
2017-09-04 13:39:01 -0300

    logging function with gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
 
 gulp.task('vet', function () {
+    log('Analysing  source with JSHint and JSCS');
+
     return gulp
         .src([
             './src/**/*.js',
@@ -14,3 +17,17 @@ gulp.task('vet', function () {
             verbose: true
         }));
 });
+
+////////////////
+
+function log(msg) {
+    if (typeof (msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
