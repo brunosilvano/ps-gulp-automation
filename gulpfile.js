8315e7bb604c9c8dce851297d7af1d0ee1824238
Bruno Silvano <bruno_705@hotmail.com>
2017-09-04 13:59:44 -0300

    added gulp plugins lazy-load with gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var util = require('gulp-util');
-var gulpprint = require('gulp-print');
-var gulpif = require('gulp-if');
 var args = require('yargs').argv;
+
+var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function () {
     log('Analysing  source with JSHint and JSCS');
@@ -14,13 +11,13 @@ gulp.task('vet', function () {
             './src/**/*.js',
             './*.js'
         ])
-        .pipe(gulpif(args.verbose, gulpprint()))
-        .pipe(jscs())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {
             verbose: true
         }))
-        .pipe(jshint.reporter('fail'));
+        .pipe($.jshint.reporter('fail'));
 });
 
 ////////////////
@@ -29,10 +26,10 @@ function log(msg) {
     if (typeof (msg) === 'object') {
         for (var item in msg) {
             if (msg.hasOwnProperty(item)) {
-                util.log(util.colors.blue(msg[item]));
+                $.util.log($.util.colors.blue(msg[item]));
             }
         }
     } else {
-        util.log(util.colors.blue(msg));
+        $.util.log($.util.colors.blue(msg));
     }
 }
